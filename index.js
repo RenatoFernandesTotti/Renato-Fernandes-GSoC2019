11c113e3d50c76889fc110bbf500befbb15ab0c7
Renato <renatoaure@outlook.com>
2019-07-11 16:13:16 -0300

    server setup for heroku

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ var cors = require('cors');
 // const pool = new sql.ConnectionPool(facensKeys)
 // pool.connect()
 
-
-app.use(cors({credentials: true, origin: 'https://gsoc-renatofernandes-2019.herokuapp.com'}))
+app.use(cors({credentials: true, origin: 'http://localhost:8181'}))
+//app.use(cors({credentials: true, origin: 'https://gsoc-renatofernandes-2019.herokuapp.com'}))
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://gsoc-renatofernandes-2019.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    //res.setHeader('Access-Control-Allow-Origin', 'https://gsoc-renatofernandes-2019.herokuapp.com');
     next();
 });
 app.use(require('./routes/router'))
--- routes/controllers/auth/auth.js
+++ routes/controllers/auth/auth.js
@@ -12,21 +12,51 @@ const LocalStrategy = require('passport-local').Strategy;
 const crypto = require('crypto')
 const maxAge = 24 * 60 * 60 * 1000
 const response = require('../../../lib/response')
+const cookieParser = require('cookie-parser')
+
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: false
 }));
-GSoC.createConnection(keys)
+router.use(cookieParser())
 
+//"Private" methods
 const authMiddleware = (req, res, next) => {
     console.log(req.isAuthenticated());
-    
+
     if (!req.isAuthenticated()) {
-      res.status(401).send('You are not authenticated')
+        res.status(401).send('You are not authenticated')
     } else {
-      return next()
+        return next()
     }
-  }
+}
+
+const setCookies = (res, exp) => {
+    return new Promise((resolve, reject) => {
+        res.cookie('valid', 'true', {
+            maxAge: exp,
+            httpOnly: false
+        })
+        console.log(res);
+
+        resolve(res)
+    })
+
+}
+
+const user = (name) => {
+    return new Promise((resolve, reject) => {
+        GSoC.getUser(name).then(result => {
+            resolve(result)
+        })
+    })
+}
+
+
+GSoC.createConnection(keys)
+
+
 
 
 
@@ -50,7 +80,7 @@ passport.use(new LocalStrategy({
                 for (i = 0; i < 100; i++) {
                     hash = crypto.createHash('sha512').update(hash).digest('hex')
                 }
-                if (hash != user.userPass) {
+                if (hash != user.userpass) {
                     return done(null, false, {
                         message: 'Invalid credentials.\n'
                     });
@@ -64,7 +94,7 @@ passport.use(new LocalStrategy({
 
 // tell passport how to serialize the user
 passport.serializeUser((user, done) => {
-    done(null, user.idUsers);
+    done(null, user.userid);
 });
 
 passport.deserializeUser((id, done) => {
@@ -104,7 +134,8 @@ router.post('/auth/login', (req, res, next) => {
 
 
         if (info) {
-
+            console.log(info.message);
+            
             return res.status(418).send(info.message)
         }
         if (err) {
@@ -112,16 +143,22 @@ router.post('/auth/login', (req, res, next) => {
             return res.status(500).send(err)
         }
         if (!user) {
-
+            console.log("pqdeus");
+            
             return res.status(401).send("User not found");
         }
         req.login(user, (err) => {
             if (err) {
-
+                console.log(err);
+                
                 return res.status(401).send(err);
             }
-            setCookies(res, maxAge - 4)
-            return res.status(200).send("ok");
+            console.log("asdas");
+            
+            setCookies(res, maxAge - 4).then(res => {
+                res.status(200).send("ok");
+            })
+            return
         })
     })(req, res, next);
 })
@@ -132,34 +169,31 @@ router.post('/auth/register', (req, res) => {
         userMail: req.body.email,
         userPass: req.body.password
     }).then(result => {
-
-        axios.post('http://localhost:8888/auth/login', {
-            name: req.body.name,
-            password: req.body.password
-        }).then(result => {
-
-            if (result.status == 200)
-                res.status(200).send("Registered!")
-        })
+        response.send(res,{
+            code:200,
+            result:result
+        })    
+    
     })
 })
 
 router.get('/auth/check', (req, res) => {
-    console.log('Log check:'+req.isAuthenticated());
-    
+    console.log('Log check:' + req.isAuthenticated());
+
     if (!req.isAuthenticated()) {
-
-
+        res.clearCookie("valid");
         res.status(400).send('You are not authenticated')
     } else {
-        res.status(200).send(req.user.userName)
+        res.status(200).send(req.user.username)
     }
 })
 
 
-router.post('/data/registersensor',authMiddleware, (req, res) => {
+router.post('/data/registersensor', authMiddleware, (req, res) => {
     var bd = req.body
-    GSoC.registerSensor(req.user.userName, {
+    console.log(req.user.username);
+    
+    GSoC.registerSensor(req.user.username, {
             name: bd.name,
             description: bd.desc,
             imgId: bd.img,
@@ -171,62 +205,70 @@ router.post('/data/registersensor',authMiddleware, (req, res) => {
             response.send(res, {
                 code: 200
             })
-        }).catch(err=>{
-            response.send(res, {
-                code: 500,
-                error:err
-            })
-        })
-})
-router.get('/getusersensors',authMiddleware,(req,res)=>{
-    GSoC.readUserSensors(req.user.userName).then(result=>{
-        console.log(result);
-        
-        response.send(res,{
-            result:result,
-            code:200
-        })
-    })
-})
-
-router.post('/data/editsensor',authMiddleware, (req, res) => {
-    var bd = req.body
-    GSoC.editSensor(bd.oldname, {
-            name: bd.name,
-            description: bd.desc,
-            imgId: bd.img,
-            lat: bd.lat,
-            lon: bd.lng,
-            unit: bd.unit
-        })
-        .then(result => {
-            response.send(res, {
-                code: 200
-            })
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err);
             
             response.send(res, {
                 code: 500,
-                error:err
+                error: err
             })
         })
 })
+router.get('/getusersensors', authMiddleware, (req, res) => {
+    GSoC.readUserSensors(req.user.username).then(result => {
+        console.log(result);
 
-const setCookies = (res, exp) => {
-    res.cookie('valid', 'true', {
-        maxAge: exp,
-        httpOnly: false
-    })
-}
-
-const user = (name) => {
-    return new Promise((resolve, reject) => {
-        GSoC.getUser(name).then(result => {
-            resolve(result)
+        response.send(res, {
+            result: result,
+            code: 200
         })
     })
-}
+})
+
+router.delete('/data/deletesensor', authMiddleware, (req, res) => {
+    console.log(req.body);
+    console.log(req.isAuthenticated());
+
+    GSoC.deleteSensor(req.body.name).then(result => {
+        response.send(res, {
+                code: 200,
+                result: result
+            })
+            .catch(err => {
+                response.send(res, {
+                    code: 500,
+                    error: err
+                })
+            })
+    })
+
+})
+
+router.post('/data/editsensor', authMiddleware, (req, res) => {
+    var bd = req.body
+    console.log(bd);
+
+    GSoC.editSensor(bd.oldname, {
+            name: bd.name,
+            description: bd.desc,
+            img: bd.img,
+            lat: bd.lat,
+            lon: bd.lng,
+            unit: bd.unit
+        })
+        .then(result => {
+            response.send(res, {
+                code: 200
+            })
+        }).catch(err => {
+            console.log(err);
+
+            response.send(res, {
+                code: 500,
+                error: err
+            })
+        })
+})
 
 
 
