971f5797cab4e289ef6b959791ea7cb0a4240c3a
Renato <renatoaure@outlook.com>
2019-07-12 00:37:10 -0300

    index.js update

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     next();
 });
 app.use(require('./routes/router'))
